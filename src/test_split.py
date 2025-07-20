a25ac34c2c7a9e6cfc2d84745d20a9969bf6bba8
MoeRon2 <fbml421@gmail.com>
2025-07-20 12:25:23 +0300

    create regex for matching images and links

--- src/test_split.py
+++ src/test_split.py
@@ -3,8 +3,6 @@ import unittest
 from textnode import TextType, TextNode
 from split_nodes_delimiter import split_nodes_delimiter
 
-print("Something!")
-
 class TestSplit(unittest.TestCase):
     def test_delimiter(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
@@ -34,7 +32,48 @@ class TestSplit(unittest.TestCase):
         print(new_nodes)
         print(new_nodes_expected)
         print("🔥🔥🔥")
+    
+    def test_italic(self):
+        node = TextNode("This is text with a *italic* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        new_nodes_expected = [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, new_nodes_expected)
+        print("🔥🔥🔥")
+        print(new_nodes)
+        print(new_nodes_expected)
+        print("🔥🔥🔥")
         
+    def test_unmatched_delimiter(self):
+        node = TextNode("This is text with a italic* word", TextType.TEXT)
+        
+        new_nodes_expected = [
+        TextNode("This is text with a ", TextType.TEXT),
+        TextNode("italic", TextType.ITALIC),
+        TextNode(" word", TextType.TEXT),
+        ]
+        print("HMMM")
+        with self.assertRaises(Exception) as cm:
+            new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+
+        self.assertIn("Invalid Markdown Syntax Unmatched delimiter", str(cm.exception))
+        print("HMMM")
+
+    def test_just_text(self):
+        node = TextNode("This is just text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        new_nodes_expected = [
+        TextNode("This is just text", TextType.TEXT),
+
+        ]
+        self.assertEqual(new_nodes, new_nodes_expected)
+        print("🔥🔥🔥")
+        print(new_nodes)
+        print(new_nodes_expected)
+        print("🔥🔥🔥")
 
 
 if __name__ == "__main__":
