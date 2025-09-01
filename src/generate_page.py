cabd9a9c7c53128e143c7116249eb1d85071f568
MoeRon2 <fbml421@gmail.com>
2025-09-02 00:16:41 +0300

    github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -17,7 +17,7 @@ def extract_title(markdown: str):
     
     raise Exception("There is no h1")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_path_content = ""
     template_path_content = ""
@@ -31,7 +31,10 @@ def generate_page(from_path, template_path, dest_path):
 
     template_path_content = template_path_content.replace("{{ Title }}", title)
     template_path_content = template_path_content.replace("{{ Content }}", html_node)
- 
+
+    template_path_content = template_path_content.replace('href="/"',f'href="{basepath}"')
+    template_path_content = template_path_content.replace('src="/"',f'src="{basepath}"')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     f = open(dest_path, "w")
@@ -40,13 +43,13 @@ def generate_page(from_path, template_path, dest_path):
 
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path,basepath)
 
